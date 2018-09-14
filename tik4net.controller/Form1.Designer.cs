8920373042b2e6ba68e54fe9324ad9ee63b853f3
THINHPVDSE62638\Firelive <pham.thinh2707@gmail.com>
2018-09-14 16:26:21 +0700

    First Commit

---
+++ tik4net.controller/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace tik4net.controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Command:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(431, 313);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(17, 357);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(431, 20);
+            this.txtCommand.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(454, 38);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(178, 339);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnSubmit_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 386);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ tik4net.controller/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tik4net.Api;
+using tik4net;
+
+namespace tik4net.controller
+{
+    public partial class Form1 : Form
+    {
+        private ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_MouseClick(object sender, MouseEventArgs e)
+        {
+            {
+                connection.OnReadRow += Connection_OnReadRow;
+                connection.OnWriteRow += Connection_OnWriteRow;
+                connection.Open(ConfigurationManager.AppSettings["host"], ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["pass"]);
+
+                List<string> commandRows = new List<string>();
+                string command;
+                Console.WriteLine("MIKROTIK CONTROLLER");
+                Console.WriteLine("Write command and press [ENTER] on empty line");
+                Console.WriteLine("Empty command + [ENTER] stops console.");
+                do
+                {
+                    command = Console.ReadLine();
+
+                    if (!string.IsNullOrWhiteSpace(command))
+                        commandRows.Add(command);
+                    else
+                    {
+                        if (commandRows.Any())
+                        {
+                            List<string> rows = new List<string>();
+                            foreach (string row in commandRows)
+                            {
+                                rows.AddRange(row.Split('|').Where(r => !string.IsNullOrEmpty(r)));
+                            }
+                            var result = connection.CallCommandSync(rows.ToArray());
+                            foreach (var resultItem in result)
+                                foreach (var word in resultItem.Words)
+                                    Console.WriteLine(word);
+
+                            commandRows.Clear();
+                        }
+                        else
+                        {
+                            break; //empty row and empty command -> end
+                        }
+
+                    }
+
+                }
+                while (true);
+            }
+        }
+
+        private static void Connection_OnWriteRow(object sender, TikConnectionCommCallbackEventArgs args)
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(">" + args.Word);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        private static void Connection_OnReadRow(object sender, TikConnectionCommCallbackEventArgs args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("<" + args.Word);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+
+
+}
