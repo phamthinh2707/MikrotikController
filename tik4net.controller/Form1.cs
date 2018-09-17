6ae6e2c0caacddad08eeb34f66dd61202f7c3630
THINHPVDSE62638\Firelive <pham.thinh2707@gmail.com>
2018-09-17 16:52:26 +0700

    Update connect ok !

--- tik4net.controller/Form1.cs
+++ tik4net.controller/Form1.cs
@@ -16,30 +16,63 @@ namespace tik4net.controller
     public partial class Form1 : Form
     {
         private ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api);
+        private string command;
+        private List<string> commandRows = new List<string>();
         public Form1()
         {
             InitializeComponent();
-            this.rtxDisplay.Text = "Mikrotik Controller";
         }
 
         private void btnSubmit_MouseClick(object sender, MouseEventArgs e)
         {
-            
+            do
+            {
+                if (this.txtHost.Text.IsNullOrWhiteSpace())
+                {
+                    this.txtCommand.Text = "Please connect to your Mikrotik Router first";
+                }
+                else
+                {
+                    command = this.txtCommand.Text;
+                    if (!string.IsNullOrWhiteSpace(command))
+                        commandRows.Add(command);
+                    else
+                    {
+                        if (commandRows.Any())
+                        {
+                            List<string> rows = new List<string>();
+                            foreach (string row in commandRows)
+                            {
+                                rows.AddRange(row.Split('|').Where(r => !string.IsNullOrEmpty(r)));
+                            }
+                            var result = connection.CallCommandSync(rows.ToArray());
+                            foreach (var resultItem in result)
+                                foreach (var word in resultItem.Words)
+                                    this.rtxDisplay.Text += word;
+
+                            commandRows.Clear();
+                        }
+                        else
+                        {
+                            break; //empty row and empty command -> end
+                        }
+                    }
+                }
+            } while (true);
         }
 
         private void btnConnect_MouseClick(object sender, MouseEventArgs e)
         {
-            
             string host = this.txtHost.Text;
             string user = this.txtUser.Text;
             string password = this.txtPassword.Text;
-            this.rtxDisplay.Text = host + user + password;
+
             connection.OnReadRow += Connection_OnReadRow;
             connection.OnWriteRow += Connection_OnWriteRow;
-            connection.Open(ConfigurationManager.AppSettings[host], ConfigurationManager.AppSettings[user], ConfigurationManager.AppSettings[password]);
-            List<string> commandRows = new List<string>();
-            string command;
+            connection.Open(host, user, password);
+
             this.btnConnect.Name = "Connected";
+
             do
             {
                 command = this.txtCommand.Text;
@@ -70,20 +103,14 @@ namespace tik4net.controller
             while (true);
         }
 
-        private static void Connection_OnWriteRow(object sender, TikConnectionCommCallbackEventArgs args)
+        private void Connection_OnWriteRow(object sender, TikConnectionCommCallbackEventArgs args)
         {
-            Console.BackgroundColor = ConsoleColor.Magenta;
-            Console.WriteLine(">" + args.Word);
-            Console.BackgroundColor = ConsoleColor.Black;
+            this.rtxDisplay.Text += args.Word;
         }
 
-        private static void Connection_OnReadRow(object sender, TikConnectionCommCallbackEventArgs args)
+        private void Connection_OnReadRow(object sender, TikConnectionCommCallbackEventArgs args)
         {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.WriteLine("<" + args.Word);
-            Console.BackgroundColor = ConsoleColor.Black;
+            this.rtxDisplay.Text += (args.Word + "\n");
         }
-
-        
     }
 }
